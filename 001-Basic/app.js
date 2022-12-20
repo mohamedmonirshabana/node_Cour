a18798c9459c4f2b9786d6cf14d2ba6195b66722
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-20 16:58:01 +0200

    add some route by from page to page

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -1,22 +1,21 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    if (url === '/') {
+const server = http.createServer((req, res) => {    
+        const url = req.url;
+        if (url === '/') {
+            // res.setHeader('Contet-Type', 'text/html');
+            res.write('<html>');
+            res.write('<head><title>Enter Message </title></head>');
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Sned</button> </form></body>');
+            res.write('</html>');
+            return res.end();
+        }
         res.setHeader('Contet-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter Message </title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Sned</button> </form></body>');
-        res.write('</html>');
-        res.end();
-    }
-    res.setHeader('Contet-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>My first Page </title></head>');
         res.write('<body><h1>Hello from my nodejs Server </h1></body>');
         res.write('</html>');
         res.end();
-
 });
 
 server.listen(3000);
