c22c331d48e5d59a9dc1e28277f786ae680eead5
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-20 15:52:37 +0200

    show request value additional value

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    console.log(req.url, req.method, req.headers, req.headers['user-agent']);
     // process.exit();
 });
 
