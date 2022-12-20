1e2f72974003a54a9e8594ebd93d9d1bc42acce0
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-20 17:11:40 +0200

    add fs and move redirect routing

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -1,13 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {    
-        const url = req.url;
+    const url = req.url;
+    const method = req.method;
         if (url === '/') {
             // res.setHeader('Contet-Type', 'text/html');
             res.write('<html>');
             res.write('<head><title>Enter Message </title></head>');
             res.write('<body><form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Sned</button> </form></body>');
             res.write('</html>');
+            return res.end();
+        }
+        if (url === '/message' && method === 'POST')
+        {
+            fs.writeFileSync('message.txt', 'Dummy');
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
             return res.end();
         }
         res.setHeader('Contet-Type', 'text/html');
