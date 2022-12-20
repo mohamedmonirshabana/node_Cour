3bdfbe2b74d5fa12fdacd4a55679cc9f90f52740
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-20 16:42:25 +0200

    add if code for url request and send Data

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -1,13 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader('Contet-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Message </title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"> <button type="submit">Sned</button> </form></body>');
+        res.write('</html>');
+        res.end();
+    }
     res.setHeader('Contet-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page </title></head>');
-    res.write('<body><h1>Hello from my nodejs Server </h1></body>');
-    res.write('</html>');
-    res.end();
+        res.write('<html>');
+        res.write('<head><title>My first Page </title></head>');
+        res.write('<body><h1>Hello from my nodejs Server </h1></body>');
+        res.write('</html>');
+        res.end();
+
 });
 
 server.listen(3000);
