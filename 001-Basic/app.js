1f753224f71f230ad4b0eb22c9f3216b8b5c4ca2
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-20 16:10:54 +0200

    add response code end with res.end()

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -1,8 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers, req.headers['user-agent']);
+    console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader('Contet-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first Page </title></head>');
+    res.write('<body><h1>Hello from my nodejs Server </h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
