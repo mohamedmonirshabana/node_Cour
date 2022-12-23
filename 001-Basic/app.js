00925596a87e5305e8943205a60ed6895160b3ce
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-23 20:35:44 +0200

    add admin for path

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -15,5 +15,8 @@ app.use(adminRoutes);
 app.use(shopRoutes);
 
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Page Not Found </h1>');
+});
 
 app.listen(3000);
--- 001-Basic/routes/admin.js
+++ 001-Basic/routes/admin.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
+router.get('/admin/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 
-router.post('/product', (req, res, next) => {
+router.post('/admin/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
