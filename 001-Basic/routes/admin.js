8469530d5588bff2fe5a9d6253a541856f7eee99
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-23 20:36:43 +0200

    change path for admin add-product

--- 001-Basic/routes/admin.js
+++ 001-Basic/routes/admin.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/admin/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 
-router.post('/admin/product', (req, res, next) => {
+router.post('/admin/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
