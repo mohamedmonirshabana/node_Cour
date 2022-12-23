6660b32e7322a9d2a3d185bfc384ec46d0abde65
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-23 21:43:26 +0200

    change for other files

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 
 const express = require('express');
+
 const bodyParser = require('body-parser');
+
+const rootDir = require('./util/path');
 
 const app = express();
 
@@ -19,7 +22,7 @@ app.use(shopRoutes);
 
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
