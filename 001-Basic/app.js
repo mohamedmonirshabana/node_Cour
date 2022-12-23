210f763791768f939afc04fddfc6c6c7640bf63e
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-23 19:44:57 +0200

    add routes folder and other Code

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -3,23 +3,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use('/add-product', (req, res, next) => {
-
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello From Express </h1>');
-});
 
 app.listen(3000);
