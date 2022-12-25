d6444becec692e275b3fa8c4961087c291db70b2
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-26 01:54:47 +0200

    add set for views folder for file with pug engine

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -10,6 +10,7 @@ const rootDir = require('./util/path');
 const app = express();
 
 app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
