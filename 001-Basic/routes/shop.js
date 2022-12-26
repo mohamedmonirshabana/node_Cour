c2ad775a41b9041d6408d0948cc1e1bdde6018a2
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-26 20:35:37 +0200

    add path for active

--- 001-Basic/routes/shop.js
+++ 001-Basic/routes/shop.js
@@ -10,7 +10,8 @@ router.get('/', (req, res, next) => {
     const products = adminData.products;
     res.render('shop', {
         prods: products,
-        docTitle: 'Shop'
+        docTitle: 'Shop',
+        path: '/'
     });
 });
 
