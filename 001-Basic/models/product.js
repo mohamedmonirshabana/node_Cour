8e23c4f5db5f01a431fb7fabf32747f971844f39
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-27 16:51:41 +0200

    fix error for cb

--- 001-Basic/models/product.js
+++ 001-Basic/models/product.js
@@ -1,18 +1,25 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProductFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t;
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
@@ -21,13 +28,7 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductFromFile(cb);
     }
 
 }
