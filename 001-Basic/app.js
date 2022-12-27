1327c09f8b889d0f8446d57b3033cda147b27854
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-27 13:24:50 +0200

    change for app to work with controller

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -16,6 +16,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const errorController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -26,12 +27,6 @@ app.use('/admin', adminRoutes.routs);
 app.use(shopRoutes);
 
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {
-        pageTitle: 'Page Not Found',
-        path: '',
-
-    })
-});
+app.use(errorController.get404);
 
 app.listen(3000);
