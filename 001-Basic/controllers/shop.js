82443b6e99957ab71015a56eb4fe7b3a22f45599
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2022-12-30 14:48:12 +0200

    add redirect for controller

--- 001-Basic/controllers/shop.js
+++ 001-Basic/controllers/shop.js
@@ -13,6 +13,7 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
     console.log(prodId);
+    res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {
