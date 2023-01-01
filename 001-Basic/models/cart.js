f7e2083cbc29889f5bfe03285efde62e289c9e98
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-01 14:06:08 +0200

    add cart for add product

---
+++ 001-Basic/models/cart.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'cart.json'
+);
+
+module.exports = class Cart {
+    // constructor() {
+    //     this.products = [];
+    //     this.totalPrice = 0;
+    // }
+
+    static addProduct(id, productPrice) {
+        // Fetch the previous cart
+        fs.readFile(p, (err, fileContent) => {
+            let cart = {
+                products: [],
+                totalQty: 0
+            };
+            if (!err) {
+                cart = JSON.parse(fileContent);
+            }
+            // Analyze the cart => find existing product
+            const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
+            const existingProduct = cart.products[existingProductIndex];
+            let updatedProduct;
+            //Add New Product/ increase Quantity
+            if (existingProduct) {
+                updatedProduct = {
+                    ...existingProduct
+                };
+                updatedProduct.qty = updatedProduct.qty + 1;
+                cart.products = [...cart.products];
+                cart.products[existingProductIndex];
+                fs.writeFile(p, JSON.stringify(cart), (err) => {
+                    console.log(err);
+                });
+            } else {
+                updatedProduct = {
+                    id: id,
+                    qty: 1
+                };
+                cart.products = [...cart.products, updatedProduct];
+            }
+            cart.totalPrice = cart.totalPrice + productPrice;
+        });
+
+    }
+}
