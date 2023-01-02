dc00d45649b0f37f5c44674107731e2885b75fed
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-03 01:35:16 +0200

    Finish Dynamic Routes

--- 001-Basic/models/cart.js
+++ 001-Basic/models/cart.js
@@ -60,6 +60,9 @@ module.exports = class Cart {
                 ...JSON.parse(fileContent)
             };
             const product = updatedCart.products.find(prod => prod.id === id);
+            if (!product) {
+                return;
+            }
             const productQty = product.qty;
             updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
             updatedCart.totalPrice = updatedCart.totalPrice - productprice * productQty;
