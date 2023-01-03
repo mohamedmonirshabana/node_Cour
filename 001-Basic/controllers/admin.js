89d2e9acf846e260c36e122fd196a745f02923bc
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-04 01:46:17 +0200

    edit edit product routes and controller

--- 001-Basic/controllers/admin.js
+++ 001-Basic/controllers/admin.js
@@ -32,18 +32,19 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, (product) => {
-        if (!product) {
-            return res.redirect('/');
-        }
-        // console.log(product);
-        res.render('admin/edit-product', {
-            pageTitle: "Edit Product",
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product
-        });
-    });
+    Product.findById(prodId)
+        .then((product) => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            // console.log(product);
+            res.render('admin/edit-product', {
+                pageTitle: "Edit Product",
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        }).catch(err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
