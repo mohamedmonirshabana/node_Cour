6e1aed16289eba766507dd49966d76603bdd30d3
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-04 02:45:48 +0200

    edit routes for update product

--- 001-Basic/controllers/admin.js
+++ 001-Basic/controllers/admin.js
@@ -1,5 +1,7 @@
+const mongodb = require('mongodb');
 const Product = require('../models/product');
 
+const ObjectId = mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
@@ -53,11 +55,13 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    const updatedProduct = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, prodId);
-    // console.log(updatedPrice, updatedDesc);
-    updatedProduct.save();
-    res.redirect('/admin/products');
-
+    const product = new Product(updatedTitle, updatedPrice, updatedDesc, updatedImageUrl, new ObjectId(prodId));
+    product.save()
+        .then(result => {
+            console.log('Updated Product!');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
