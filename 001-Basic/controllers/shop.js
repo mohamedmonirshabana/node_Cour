3fddff3f71d0fc888b5b172bac84d81e1793392c
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-05 19:47:05 +0200

    add show my Cart but there is a  problem i have to fix it

--- 001-Basic/controllers/shop.js
+++ 001-Basic/controllers/shop.js
@@ -43,26 +43,13 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-    // Cart.getCart(cart => {
-    //     Product.fetchAll(products => {
-    //         const cartProducts = [];
-    //         for (product of products) {
-    //             const cartProductData = cart.products.find(prod => prod.id === product.id);
-    //             if (cartProductData) {
-    //                 cartProducts.push({
-    //                     productData: product,
-    //                     qty: cartProductData.qty
-    //                 });
-    //             }
-    //         }
-    //         // console.log(cartProducts);
-    //         res.render('shop/cart', {
-    //             path: '/cart',
-    //             pageTitle: 'Your Cart',
-    //             products: cartProducts
-    //         });
-    //     });
-    // });
+    req.user.getCart().then(products => {
+        res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+        });
+    }).catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
--- 001-Basic/models/user.js
+++ 001-Basic/models/user.js
@@ -46,6 +46,27 @@ class User {
         }).then(result => console.log(result)).catch(err => console.log(err));
     }
 
+    getCart() {
+        const db = getDb();
+        const productIds = this.cart.items.map(i => {
+            return i.productId
+        })
+        return db.collection('products').find({
+            _id: {
+                $in: [productIds]
+            }
+        }).toArray().then(products => {
+            return products.map(p => {
+                return {
+                    ...p,
+                    quantity: this.cart.items.find(i => {
+                        return i.productId.toString() === p._id.toString();
+                    }).quantity
+                };
+            })
+        });
+    }
+
     static findById(userId) {
         const db = getDb();
         const user_id = new ObjectId(userId);
