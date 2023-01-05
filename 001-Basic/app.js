885c932e69d32f66a2a555e917a657a6d89f852a
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-05 12:20:11 +0200

    add user to a program

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const rootDir = require('./util/path');
 
@@ -24,6 +25,15 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    User.findById("63b6a274e9e75e642bb4e97f")
+        .then(user => {
+            req.user = user;
+            next();
+        }).catch(err => console.log(err));
+    next();
+});
 
 app.use('/admin', adminRoutes.routs);
 app.use(shopRoutes);
