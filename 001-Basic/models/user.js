16575d1e3c4d8bcd22bbfbc72e99983b218d9c7d
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-05 14:51:14 +0200

    add cart for user

--- 001-Basic/models/user.js
+++ 001-Basic/models/user.js
@@ -4,14 +4,36 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.username = username;
         this.email = email;
+        this.cart = cart; // {items:[]}
+        this._id = id ? new ObjectId(id) : null;
     }
 
     save() {
         const db = getDb();
         return db.collection('users').insertOne(this).then(result => console.log(result)).catch(err => console.log(err));
+    }
+
+    addToCart(product) {
+        // const cartProduct = this.cart.items.findIndex(cp => {
+        //     cp._id === product._id;
+        // });
+        const updatedCart = {
+            items: [{
+                ...product,
+                quantity: 1
+            }]
+        };
+        const db = getDb();
+        return db.collection('users').updateOne({
+            _id: this._id
+        }, {
+            $set: {
+                cart: updatedCart
+            }
+        }).then(result => console.log(result)).catch(err => console.log(err));
     }
 
     static findById(userId) {
