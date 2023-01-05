23481c0c99083fa5a1a217120b8b7a94f293e968
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-05 15:18:19 +0200

    add cart for user quantity 1

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
 
     User.findById("63b6a274e9e75e642bb4e97f")
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         }).catch(err => console.log(err));
 });
