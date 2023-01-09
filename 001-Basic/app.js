4bd9609f52666dde9356d33f6503f1a36771fa62
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-09 11:57:32 +0200

    add mongoose for user and add user for middleware

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 // const rootDir = require('./util/path');
 
@@ -26,13 +26,13 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById("63b6a274e9e75e642bb4e97f")
-//         .then(user => {
-//             req.user = new User(user.name, user.email, user.cart, user._id);
-//             next();
-//         }).catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById("63bbe4312707028454ad44a6")
+        .then(user => {
+            req.user = user;
+            next();
+        }).catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes.routs);
 app.use(shopRoutes);
@@ -40,8 +40,21 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://medo:ASD-123456@cluster0.wongn.mongodb.net/ShopDB').then(result => {
-    app.listen(3000);
-}).catch(err => {
-    console.log(err);
-});
+mongoose.connect('mongodb+srv://medo:ASD-123456@cluster0.wongn.mongodb.net/ShopDB')
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Medo',
+                    email: 'mohamed@google.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
+        app.listen(3000);
+    }).catch(err => {
+        console.log(err);
+    });
