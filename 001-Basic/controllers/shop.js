53f248d3ee699d59e4c309b94ba9e93a410ecb49
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-10 15:31:01 +0200

    add product for order to get all product Data

--- 001-Basic/controllers/shop.js
+++ 001-Basic/controllers/shop.js
@@ -83,7 +83,9 @@ exports.postOrder = (req, res, next) => {
             const products = user.cart.items.map(i => {
                 return {
                     quantity: i.quantity,
-                    product: i.productId
+                    product: {
+                        ...i.productId._doc
+                    }
                 }
             });
             const order = new Order({
