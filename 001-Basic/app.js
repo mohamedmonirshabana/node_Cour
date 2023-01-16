5333a85888bf7fa596543eea5ea2365e809d46f3
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-16 14:36:04 +0200

    add some file for auth

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -19,6 +19,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 
 app.use(bodyParser.urlencoded({
@@ -36,6 +37,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes.routs);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 
 app.use(errorController.get404);
