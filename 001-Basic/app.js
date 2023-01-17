fdf812e2de70082def58fd69839d693d0de67e1d
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-17 17:39:09 +0200

    session with Cookie

--- 001-Basic/app.js
+++ 001-Basic/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 
@@ -26,6 +27,14 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 10
+    }
+}));
 
 app.use((req, res, next) => {
     User.findById("63bbe4312707028454ad44a6")
