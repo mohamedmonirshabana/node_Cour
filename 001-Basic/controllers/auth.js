e020186c156fd9e9a180fd419034f16674130d08
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-17 14:59:57 +0200

    add Cookie for browser

--- 001-Basic/controllers/auth.js
+++ 001-Basic/controllers/auth.js
@@ -1,13 +1,20 @@
 exports.getLogin = (req, res, next) => {
+    let isLoggedIn;
+    if (req.get('Cookie') !== undefined) {
+        isLoggedIn = req.get('Cookie').split('=')[1];
+    } else {
+        isLoggedIn = false;
+    }
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: isLoggedIn
     });
 };
 
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    // req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggedIn=true');
     res.redirect('/');
 };
