ba64e501f8acdc6200079ddca2bc40edb766275f
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-29 15:11:48 +0200

    add user signup for user Data

--- 001-Basic/controllers/auth.js
+++ 001-Basic/controllers/auth.js
@@ -37,7 +37,34 @@ exports.postLogin = (req, res, next) => {
     }).catch(err => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const username = email.split('@')[0];
+    const confirmPassword = req.body.confirmPassword;
+    User.findOne({
+            email: email
+        }).then(userDoc => {
+            if (userDoc) {
+                return res.redirect('/signup');
+            }
+            const user = new User({
+                name: username,
+                email: email,
+                password: password,
+                cart: {
+                    items: []
+                }
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.redirect('/login');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy((err) => {
