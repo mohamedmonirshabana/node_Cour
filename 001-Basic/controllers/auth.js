e47cf35f2749b93967f6bd25363cdc78a1e61465
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-01-29 16:30:03 +0200

    add some feature for auth bcryptjs

--- 001-Basic/controllers/auth.js
+++ 001-Basic/controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -48,10 +50,14 @@ exports.postSignup = (req, res, next) => {
             if (userDoc) {
                 return res.redirect('/signup');
             }
+            return bcrypt.hash(password, 12);
+
+        })
+        .then(hashedPassword => {
             const user = new User({
                 name: username,
                 email: email,
-                password: password,
+                password: hashedPassword,
                 cart: {
                     items: []
                 }
