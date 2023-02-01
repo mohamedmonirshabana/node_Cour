6dd068c4e88e951c9fd9a76ad240ae12319fb1a5
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-02-01 16:29:10 +0200

    add for auth if flash has Data

--- 001-Basic/controllers/auth.js
+++ 001-Basic/controllers/auth.js
@@ -10,10 +10,16 @@ exports.getLogin = (req, res, next) => {
     //     isLoggedIn = false;
     // }
     // console.log(req.session.isLoggedIn);
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null;
+    }
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        errorMessage: req.flash('error')
+        errorMessage: message
     });
 };
 
